52531102e4f530fb419dc4cf73c0057f103d38a7
EC2 Default User <jlvdantry@hotmail.com>
2019-10-12 16:29:54 -0500

    se cambia el title y los titulos

--- titulos.php
+++ titulos.php
@@ -1,15 +1,15 @@
 <?php
-echo " <LINK id=estilo REL=StyleSheet HREF='pupanint.css' TYPE='text/css' MEDIA=screen>\n";
+echo "<LINK id=estilo REL=StyleSheet HREF='pupanint.css' TYPE='text/css' MEDIA=screen>\n";
 echo "<table class=tabletit id=encabezado width=60% align=center>";
 echo "<tr align=center>";
-echo "<td rowspan=3 width=20%><img src='img/cdmx_03.png' height='65' </td>";
-echo "<td><b>Ciudad de Mexico</b></td>";
-echo "<td rowspan=3 width=20%><img src='img/styfe.png' height='65' </td>";
+echo "<td rowspan=3 width=20%><img src='img/logo1.png' height='65' alt='logotipo1' </td>";
+echo "<td><b>Titulo1</b></td>";
+echo "<td rowspan=3 width=20%><img src='img/logo2.png' height='65' alt='logotipo2' </td>";
 echo "<tr align=center>";
-echo "<td><b>Secretaria del Trabajo y Fomento al Empleo</b></td>";
+echo "<td><b>Titulo2</b></td>";
 echo "</tr>";
 echo "<tr align=center>";
-echo "<td><b>Secretaria Particular</b></td>";
+echo "<td><b>Titulo3</b></td>";
 echo "</tr>";
 echo "</table>";
 ?>
