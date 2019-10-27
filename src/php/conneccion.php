f1ed12ce98c92814cd58568f294a948b68a7ef1f
EC2 Default User <jlvdantry@hotmail.com>
2019-10-27 14:12:29 -0600

    el usuariote temporal se le agrega el nombre de la base de datos

--- src/php/conneccion.php
+++ src/php/conneccion.php
@@ -1,7 +1,7 @@
 <?php
 session_cache_expire(60);  ## se establece el limite de la sesion de php en 60 min
 session_start(); 
-
+require_once("idmenus.php");
 ##  hay que checar porque hay veces que entra la clave temporal, como que hay un limite y se pierde la session
 $wlhost='localhost';
 $wldbname=BD;
@@ -16,9 +16,7 @@ else
     {	$connection = pg_connect("host=$wlhost dbname=BD user='jlv' password='888aDantryR' port=$wlport") or die("Error en el cron");	}
     else
     {	
-//        $_SESSION["parametro1"]='temporal';
-        $connection = pg_connect("host=$wlhost	dbname=BD user='temporal' password=PWD_USER_TMP port=$wlport") or die("Error con clave temporal1");
-//        pg_exec($connection,"set client_encoding to 'latin1'");
+        $connection = pg_connect("host=$wlhost	dbname=".BD." user=tmp_".BD." password=".PWD_USER_TMP." port=$wlport") or die("Error con clave temporal1");
     }
 }
 ?>
--- src/sh/crea_forapi.sh
+++ src/sh/crea_forapi.sh
@@ -13,13 +13,14 @@ export PGPASSWORD=$3
 cat > $0.sql << fin
 drop database $1;
 drop user $2 ;
-drop user Temporal_forapi;
-drop role temporal;
 drop role admon;
+drop role temporalg;
+drop user $2 ;
 create user $2 with password '$3' superuser;
 create database $1 owner $2;
 comment on database $1 is 'Formas rapidas';
-create user Temporal_forapi with password '$3' ;
+drop user tmp_$2 ;
+create user Temporal_$1 with password '$3' ;
 create role temporalg;
 create role admon;
 create schema forapi;
@@ -28,13 +29,13 @@ psql -U postgres < $0.sql > tmp/$mn.log
 psql $1 -U $2 -h localhost  < src/bd/forapi_esquema.sql >> tmp/$mn.log
 psql $1 -U $2 -h localhost  < src/bd/forapi_insert.sql >> tmp/$mn.log
 cat > $0.sql << fin
-insert into forapi.cat_usuarios (usename,nombre,id_tipomenu,password,estatus) values ('Temporal1','temporal',1,'$3',1);
+insert into forapi.cat_usuarios (usename,nombre,id_tipomenu,password,estatus) values ('tmp_$1','temporal',1,'$3',1);
 insert into forapi.cat_usuarios (usename,nombre,id_tipomenu,password,menu,estatus) values ('$2','$2',1,'$3',
 (select idmenu from forapi.menus where descripcion='Mtto a usuarios'),1);
-insert into forapi.cat_usuarios_pg_group(usename,groname) values ('Temporal1','temporalg');
+insert into forapi.cat_usuarios_pg_group(usename,groname) values ('tmp_$1','temporalg');
 insert into forapi.cat_usuarios_pg_group(usename,groname) values ('$2','admon');
 delete from forapi.menus_pg_tables where nspname not in ('forapi','pg_catalog','estadistica','public');
-select forapi.autoriza_usuario('Temporal_forapi');
+select forapi.autoriza_usuario('tmp_$1');
 select forapi.autoriza_usuario('$2');
 delete from pg_authid where rolcanlogin=false and rolname not in ('admon','temporalg');
 fin
@@ -58,10 +59,4 @@ psql -t $1 -U $2 -h localhost  < $0.sql  > ./src/php/idmenus.php
 echo "creo constantes"
 sed -i -e "s/ //g" ./src/php/idmenus.php
 tail -n 1 "./src/php/idmenus.php" | wc -c | xargs -I {} truncate "./src/php/idmenus.php" -s -{}
-##sed -i -e "s/wldbname='forapi1.1'/wldbname='$1'/g" -e "s/password='Temporal_forapi'/password='$3'/g" ./src/php/conneccion.php
 echo "cambio variales de la base"
-##mkdir upload_ficheros
-##chown -R ec2-user:www upload_ficheros
-##mkdir ficheros
-##chown -R ec2-user:www ficheros
-##rm $0.sql
