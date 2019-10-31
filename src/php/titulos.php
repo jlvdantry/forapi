12c62f5d9fda7047cb3917ff09a402657d6cb48b
EC2 Default User <jlvdantry@hotmail.com>
2019-10-31 09:43:52 -0600

    se corrige titulos

--- src/php/titulos.php
+++ src/php/titulos.php
@@ -1,14 +1,3 @@
 <?php
-echo "<table class=tabletit id=encabezado width=60% align=center>";
-echo "<tr align=center>";
-echo "<td rowspan=3 width=20%><img src='img/logo1.png' height='65' alt='logotipo1' </td>";
-echo "<td><b>Titulo1</b></td>";
-echo "<td rowspan=3 width=20%><img src='img/logo2.png' height='65' alt='logotipo2' </td>";
-echo "<tr align=center>";
-echo "<td><b>Titulo2</b></td>";
-echo "</tr>";
-echo "<tr align=center>";
-echo "<td><b>Titulo3</b></td>";
-echo "</tr>";
-echo "</table>";
+echo "<h1>Encabezado</h1>";
 ?>
