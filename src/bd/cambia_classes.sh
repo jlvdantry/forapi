d77bd4fa7c8e3620c72d694cfafe3526c9fc7b4e
EC2 Default User <jlvdantry@hotmail.com>
2019-11-02 18:01:50 -0600

    estilo de upload archivos

--- src/bd/cambia_classes.sh
+++ src/bd/cambia_classes.sh
@@ -4,8 +4,8 @@ cat > $0.sql << fin
 --select * from forapi.menus_campos where idmenu=46 and eshidden=false order by htmltable,orden
 alter user tmp_jc9 with CREATEUSER CREATEROLE;
 fin
-psql jc104 -U jc104 -h localhost  <  src/bd/tablas.sql
-##psql jc9 -U jc9  -h localhost < $0.sql
+##psql jc104 -U jc104 -h localhost  <  src/bd/tablas.sql
+psql jc104 -U jc104  -h localhost < src/bd/cambio_menus_columnas.sql
 ##psql jc9 -U jc9 -h localhost  <  src/bd/menus_scripts.sql
 ##psql jc9 -U jc9 -h localhost  <  src/bd/cambio_menus.sql
 ##psql inicio -U inicio -h localhost  <   src/bd/crea_funcion_de_columnas_campos_menus.sql
